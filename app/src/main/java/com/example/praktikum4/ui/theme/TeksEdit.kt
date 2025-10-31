f4d923e2c76838390f4c275d0e3c33d1f4e40a2a
abimanyusatriaa <adji3890@gmail.com>
2025-10-31 09:15:19 +0700

    membuat composable

---
+++ app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum4.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+
